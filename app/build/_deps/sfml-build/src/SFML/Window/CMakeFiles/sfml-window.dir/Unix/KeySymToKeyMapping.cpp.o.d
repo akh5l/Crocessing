a58b64e27da03915f7f84d1bf09976f72004cf66
akh5l <160544848+akh5l@users.noreply.github.com>
2025-06-19 18:28:12 +0100

    Delete app/build directory

--- app/build/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToKeyMapping.cpp.o.d
+++
@@ -1,9 +0,0 @@
-_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToKeyMapping.cpp.o: \
- /home/akhilsoman/Documents/VS\ Code/c++/imgui\ test/app/build/_deps/sfml-src/src/SFML/Window/Unix/KeySymToKeyMapping.cpp \
- /usr/include/stdc-predef.h \
- /home/akhilsoman/Documents/VS\ Code/c++/imgui\ test/app/build/_deps/sfml-src/src/SFML/Window/Unix/KeySymToKeyMapping.hpp \
- /home/akhilsoman/Documents/VS\ Code/c++/imgui\ test/app/build/_deps/sfml-src/include/SFML/Window/Keyboard.hpp \
- /home/akhilsoman/Documents/VS\ Code/c++/imgui\ test/app/build/_deps/sfml-src/include/SFML/Window/Export.hpp \
- /home/akhilsoman/Documents/VS\ Code/c++/imgui\ test/app/build/_deps/sfml-src/include/SFML/Config.hpp \
- /usr/include/X11/X.h /usr/include/X11/keysym.h \
- /usr/include/X11/keysymdef.h
